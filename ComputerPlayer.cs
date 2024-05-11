546491c7479782f0d15d5214ae359a473d33ffcc
Ionselat <ionselat@gmail.com>
2024-05-11 19:30:26 -0400

    Add project files.

---
+++ ComputerPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public class ComputerPlayer : IPlayer
+    {
+        private Random rng = new Random();
+        public Choice GetChoice()
+        {
+            Choice p2 = (Choice)rng.Next(0, 3);
+            return p2;
+        }
+    }
+}
